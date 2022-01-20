048c3f9abf92a9c0d5111c5ecf235883eeb330d0
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-20 14:37:14 +0530

    Add project files.

---
+++ ConsoleApp1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context=new CustomerContext())
+            {
+                List<Customers> custom = context.Customers.ToList();
+                foreach(var item in custom)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            
+        }
+    }
+}
